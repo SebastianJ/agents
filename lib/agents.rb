14f072f3e817125c24f4d59255b8eeda08026c85
Sebastian <sebastian.johnsson@gmail.com>
2018-07-11 21:48:35 +0200

    Add additional helper methods

--- lib/agents.rb
+++ lib/agents.rb
@@ -3,9 +3,23 @@ require "agents/constants"
 
 module Agents
   
-  def self.random_user_agent(*args)
-    args    =   (args.nil? || args.empty?) ? [:desktop] : args
-    ::Agents::Constants::USER_AGENTS.dig(*args&.collect(&:to_sym))&.sample
+  class << self
+    def random_user_agent(*args)
+      args    =   (args.nil? || args.empty?) ? [:desktop] : args
+      ::Agents::Constants::USER_AGENTS.dig(*args&.collect(&:to_sym))&.sample
+    end
+    
+    def runs_ios?(user_agent)
+      is_iphone?(user_agent) || is_ipad?(user_agent)
+    end
+  
+    def is_iphone?(user_agent)
+      !(user_agent =~ /iphone|cpu\siphone/i).nil?
+    end
+  
+    def is_ipad?(user_agent)
+      !(user_agent =~ /ipad/i).nil?
+    end
   end
   
 end
--- spec/agents_spec.rb
+++ spec/agents_spec.rb
@@ -2,8 +2,20 @@ RSpec.describe Agents do
   it "has a version number" do
     expect(Agents::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+  
+  describe "randomizes a correct iPhone user agent" do
+    let(:user_agent) { Agents.random_user_agent(:phones, :iphone) }
+    
+    it "should correctly identify the user agent as being an iOS device" do
+      expect(Agents.runs_ios?(user_agent)).to eq(true)
+    end
+  
+    it "should correctly identify the user agent as being an iPhone" do
+      expect(Agents.is_iphone?(user_agent)).to eq(true)
+    end
+  
+    it "should correctly identify the user agent as not being an iPad" do
+      expect(Agents.is_ipad?(user_agent)).to eq(false)
+    end
   end
 end
